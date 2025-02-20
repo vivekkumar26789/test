8d34e96795ce76766191fb827885e371c582b6a1
pray63764 <pray63764@gmail.com>
2025-02-20 08:31:20 +0530

    Implemented dev8 changes

--- IM_CONSOLE_PROJECT/Globals.cs
+++ IM_CONSOLE_PROJECT/Globals.cs
@@ -4,5 +4,6 @@ namespace IM_CONSOLE_PROJECT
     public static class Globals
     {
         public static string ENVIRONMENT_CODE = "UAT";
+        public static int numVal = 10;
     }
 }
--- IM_CONSOLE_PROJECT/dev5.cs
+++ IM_CONSOLE_PROJECT/dev5.cs
@@ -3,6 +3,7 @@ namespace IM_CONSOLE_PROJECT
 {
     public class dev5
     {
-        public static string myVal = "dev5 changes";
+        public static string myVal8 = "dev8 changes";
+        private int x = 20;
     }
 }
